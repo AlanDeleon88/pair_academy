d42931897535245bb524e977c89c70213d4412ef
AlanDeleon88 <19deleon88@gmail.com>
2023-09-21 20:18:32 -0700

    added user auth middleware and connected it to the api routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,43 @@
 const router = require('express').Router()
 
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth.js')
+const { User } = require('../../db/models')
 
-router.post('/test', function(req, res){
-    res.json({requestBody: req.body})
-})
+
+
+router.use(restoreUser)
+
+
+
+
+//! AUTH middleware testing routes
+// router.get(
+//     '/restore-user',
+//     (req, res) =>{
+//         return res.json(req.user)
+//     }
+// )
+
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) =>{
+//         return res.json(req.user)
+//     }
+// )
+
+// router.get('/set-token-cookie', async (_req, res) =>{
+//     const user = await User.findOne({
+//         where:{
+//             username: 'Demo-lition'
+//         }
+//     })
+//     setTokenCookie(res, user)
+//     return res.json({user: user})
+// })
+// router.post('/test', function(req, res){
+//     res.json({requestBody: req.body})
+// })
 
 
 
