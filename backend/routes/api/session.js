cba1fe9598c42c1224b5dcc35e407a966efc336f
AlanDeleon88 <19deleon88@gmail.com>
2023-10-09 16:43:06 -0700

    added validation middleware and express-validate middleware to login endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,7 +3,21 @@ const router = express.Router();
 
 const {setTokenCookie, restoreUser} = require('../../utils/auth');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation')
+
 const {User} = require('../../db/models');
+
+const validateLogin = [
+    check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valide email or username'),
+    check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password'),
+    handleValidationErrors
+]
 
 
 router.get(
@@ -22,9 +36,10 @@ router.get(
     }
 )
 
-
+//Log in
 router.post(
     '/',
+    validateLogin,
     async(req, res, next) =>{
         const {credential, password} = req.body;
 
