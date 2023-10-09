342402339afbcef5cca4dec8be514f7444de2919
AlanDeleon88 <19deleon88@gmail.com>
2023-10-09 16:28:14 -0700

    added get session user endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,6 +6,23 @@ const {setTokenCookie, restoreUser} = require('../../utils/auth');
 const {User} = require('../../db/models');
 
 
+router.get(
+    '/',
+    restoreUser,
+    (req, res) =>{
+        const {user} = req;
+        if(user){
+            return res.json({
+                user: user.toSafeObject()
+            })
+        }
+        else{
+            return res.json({user: null});
+        }
+    }
+)
+
+
 router.post(
     '/',
     async(req, res, next) =>{
